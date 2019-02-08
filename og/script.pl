57335567dd7fc06b6fa902757dca4f5bc7ab257c
Usain-mbp <wolf257@users.noreply.github.com>
2019-02-08 14:58:10 +0100

    modif gitignore

--- og/script.pl
+++ og/script.pl
@@ -4,6 +4,8 @@
 # - gros bloc contenant toutes les balises.
 # - une balise sur 2 lignes.
 
+use XML::RSS;
+
 sub all_lines_into_one {
     my $compteur = 0;
     my $ens_ligne;
@@ -27,6 +29,7 @@ sub write_xml_tail {
     print $fic "</racine>";
 }
 
+
 # 1/ Lecture fichier rss en 1 seule ligne (L)
 my $fic = shift(@ARGV);
 
@@ -41,21 +44,55 @@ open(FICOUTXML, ">:encoding(utf8)", "./sortie-xml.xml") or die("message3");
 write_xml_header(FICOUTXML);
 
 # 2/ find in L les occurrences de titre et de description
+
 my $motif = "<item>.*?<title>([^<]*)<\/title>.*?<description>([^<]*)</description>.*?</item>";
 
 my $num_art = 0;
 while ($ens_ligne =~ /$motif/g) {
-    print FICOUT "$1\n";
-    print FICOUT "$2\n\n";
+    my ($titre, $description) = ($1, $2);
 
-    print FICOUTXML "\t<titre num_art=\"$num_art\"> $1 </titre>\n";
-    print FICOUTXML "\t<description num_art=\"$num_art\"> $2 </description>\n\n";
+    ($titre, $description) = &cleaning($titre, $description);
+
+    print FICOUT "$titre\n";
+    print FICOUT "$description\n\n";
+
+    print FICOUTXML "\t<titre num_art=\"$num_art\"> $titre </titre>\n";
+    print FICOUTXML "\t<description num_art=\"$num_art\"> $description </description>\n\n";
 
     $num_art++;
 }
+
+# my $motif = "<item>.*?<title>([^<]*)<\/title>.*?<description>([^<]*)</description>.*?</item>";
+#
+# my $num_art = 0;
+# while ($ens_ligne =~ /$motif/g) {
+#     my ($titre, $description) = ($1, $2);
+#     # my $description = $2;
+#
+#     ($titre, $description) = &cleaning($titre, $description);
+#
+#     print FICOUT "$titre\n";
+#     print FICOUT "$description\n\n";
+#
+#     print FICOUTXML "\t<titre num_art=\"$num_art\"> $titre </titre>\n";
+#     print FICOUTXML "\t<description num_art=\"$num_art\"> $description </description>\n\n";
+#
+#     $num_art++;
+# }
 
 write_xml_tail(FICOUTXML);
 
 # Close files
 close(FICOUT);
 close(FICOUTXML);
+
+#---------------------------------------------------------
+sub cleaning {
+    # tous les args sont dans @_
+    my ($t, $d) = @_;
+
+    $t .= ".";
+    $d =~ s/&#38;#39;/'/g;
+
+    return $t, $d;
+}
